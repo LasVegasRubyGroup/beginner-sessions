8c3c251c968ffc66ba2b3595336dfa0937a059e2
Gabe Evans <gabe@ga.be>
2013-06-27 09:25:47 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Bloggy
+
+Tonight we created the base for a small, personal blog on Rails 4.0.0. We
+covered:
+
+* Basic routing
+* Generating models and controllers
+* Rendering a partial for each item in a collection
+* Using Rails' form builder to create a small form
+* Handling parameters in our controller actions
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bloggy::Application.config.secret_key_base = 'f7223d6f31f89cc4bac37c2542cecc939cee34cb4a8018d3d3a94b6d3191843aeb018e05f06187a2ba2761b1d37d43a542831bbc3706b865d5474398f62499d3'
